35e7ecbd8414bb3234af0200511eaa6a80ea415f
marc <meopsbronte@gmail.com>
2016-12-19 09:55:41 +0100

    Marc-Antoine 9:55

--- src/main/java/org/formation/service/ConseillerService.java
+++ src/main/java/org/formation/service/ConseillerService.java
@@ -11,35 +11,35 @@ public class ConseillerService {
 	@Autowired
 	IConseillerDao conseillerDao;
 
-	void persist(Conseiller conseiller) throws Exception {
+	public void persist(Conseiller conseiller) throws Exception {
 		conseillerDao.persist(conseiller);
 	}
 
-	void merge(Conseiller conseiller) throws Exception {
+	public void merge(Conseiller conseiller) throws Exception {
 		conseillerDao.merge(conseiller);
 	}
 
-	void remove(Conseiller id) throws Exception {
+	public void remove(Conseiller id) throws Exception {
 		conseillerDao.remove(id);
 	}
 
-	Conseiller findById(Conseiller id) throws Exception {
+	public Conseiller findById(Conseiller id) throws Exception {
 		return conseillerDao.findById(id);
 	}
 
-	List<Conseiller> findAll() throws Exception {
+	public List<Conseiller> findAll() throws Exception {
 		return conseillerDao.findAll();
 	}
 
-	List<Conseiller> findByProperty(String prop, Object val) throws Exception {
+	public List<Conseiller> findByProperty(String prop, Object val) throws Exception {
 		return conseillerDao.findByProperty(prop, val);
 	}
 
-	List<Conseiller> findInRange(int firstResult, int maxResults) throws Exception {
+	public List<Conseiller> findInRange(int firstResult, int maxResults) throws Exception {
 		return conseillerDao.findInRange(firstResult, maxResults);
 	}
 
-	long count() throws Exception {
+	public long count() throws Exception {
 		return conseillerDao.count();
 	}
 }
