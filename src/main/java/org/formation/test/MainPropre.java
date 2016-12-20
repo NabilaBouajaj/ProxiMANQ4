0723e666598d32b3244354684656f976c4c4a046
marc <meopsbronte@gmail.com>
2016-12-20 09:49:29 +0100

    enfin la fin de la création de client

--- src/main/java/org/formation/test/MainPropre.java
+++ src/main/java/org/formation/test/MainPropre.java
@@ -2,7 +2,9 @@ package org.formation.test;
 
 import org.formation.model.Adresse;
 import org.formation.model.Client;
+import org.formation.model.Conseiller;
 import org.formation.service.ClientService;
+import org.formation.service.ConseillerService;
 import org.formation.service.IClientService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,5 +17,9 @@ public class MainPropre {
 		ClientService clientService = applicationContext.getBean("clientService", ClientService.class);
 		Client client = new Client("nom", "jidrs", "ovs", new Adresse());
 		clientService.createClient(client);
+
+		ConseillerService conseillerService = applicationContext.getBean("conseillerService", ConseillerService.class);
+		Conseiller conseiller = new Conseiller("Dieu", "Notre père", "dieu", "dieu");
+		conseillerService.persist(conseiller);
 	}
 }
